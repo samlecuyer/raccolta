5ed236db229d9acdbc7d62ebaf84f71d0eb03e8f
Sam L'ecuyer <slecuyer@186590d468e3.ant.amazon.com>
2017-10-11 09:31:10 -0700

    tweak

--- gulpfile.js
+++ gulpfile.js
@@ -5,11 +5,11 @@ var sass = require('gulp-sass');
 gulp.task('html', () => 
   gulp.src('views/index.pug')
     .pipe(pug())
-    .pipe(gulp.dest('build/')));
+    .pipe(gulp.dest('dist/')));
 
 gulp.task('css', () => 
   gulp.src('styles/raccolta.scss')
     .pipe(sass())
-    .pipe(gulp.dest('build/public')));
+    .pipe(gulp.dest('dist/public')));
 
 gulp.task('default', ['html', 'css']);
